c670da59d1b058926d725cd644e54462b3051f3b
iOSyan <ecsage@yikesiqidemacbook-air.local>
2016-08-24 17:19:12 +0800

    Initial Commit

---
+++ VehicleRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VehicleRecorder
+//
+//  Created by ecsage on 16/8/24.
+//  Copyright © 2016年 auvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VehicleRecorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VehicleRecorder
+//
+//  Created by ecsage on 16/8/24.
+//  Copyright © 2016年 auvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
